1f9d9871b36689465db0099ef547b45ffdd2a0f6
Chocolate1999 <2816778271@qq.com>
2020-04-23 22:45:02 +0800

    Site updated: 2020-04-23 22:44:54

--- js/utils.js
+++ js/utils.js
@@ -124,3 +124,55 @@ window.throttle = throttle
 window.isMobile = isMobile
 
 window.loadScript = loadScript
+
+
+
+
+
+
+
+/* 微信推送 */
+var title1="text=超逸の博客又有新评论啦~！--by lionkk"
+var SCKEY_Server="SCU95250T94b4dfcb89254a0f1fa9e239c88e76bc5ea1a06689e98"
+var ValineButton=document.getElementsByClassName("vsubmit vbtn")[0];
+function send_valine_Server(){
+    var text="desp=";
+    var pagename=document.title;
+    var wz=pagename.indexOf('|');
+    var res=pagename.substring(0,wz);
+    var pageurl=document.URL;
+    var ptime=new Date();
+    var vnick=document.getElementsByClassName("vnick vinput")[0].value;
+    var vmail=document.getElementsByClassName("vmail vinput")[0].value;
+    var vlink=document.getElementsByClassName("vlink vinput")[0].value;
+    var veditor=document.getElementsByClassName("veditor vinput")[0].value;
+    var data=text+"|昵称："+"|邮箱："+"|网站地址："+"|当前页面："+"|评论内容："+"|跳转链接："+"|评论时间"+"\n"+"|----|----|----|----|"+"\n"+"|   "+vnick+"   |   "+vmail+"  |  "+vlink+"|   "+res+"| "+veditor+"| "+pageurl+"|" +ptime.toLocaleString()+"|";
+    var httpRequest = new XMLHttpRequest();//第一步：创建需要的对象
+    httpRequest.open('POST', 'https://sc.ftqq.com/'+SCKEY_Server+'.send', true); //第二步：打开连接
+    httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");//设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头）
+    httpRequest.send(title1+"&"+data);//发送请求 将情头体写在send中
+};
+ValineButton.onclick=send_valine_Server;
+
+
+/* QQ推送 */
+var title2="msg=超逸の博客又有新评论啦~！--by lionkk\n"
+var SCKEY_Qmsg="https://qmsg.zendee.cn:443/send/a83c45c4f6955fee3e3cbc04b32abeea.html"
+var ValineButton=document.getElementsByClassName("vsubmit vbtn")[0];
+function send_valine_Qmsg(){
+    var pagename=document.title;
+    var wz=pagename.indexOf('|');
+    var res=pagename.substring(0,wz);
+    var pageurl=document.URL;
+    var ptime=new Date();
+    var vnick=document.getElementsByClassName("vnick vinput")[0].value;
+    var vmail=document.getElementsByClassName("vmail vinput")[0].value;
+    var vlink=document.getElementsByClassName("vlink vinput")[0].value;
+    var veditor=document.getElementsByClassName("veditor vinput")[0].value;
+    var data="昵称："+vnick+"\n邮箱："+vmail+"\n网站地址："+vlink+"\n当前页面："+pagename+"\n评论内容："+veditor+"\n跳转链接："+pageurl+"\n评论时间"+ptime.toLocaleString();
+    var httpRequest = new XMLHttpRequest();//第一步：创建需要的对象
+    httpRequest.open('POST',SCKEY_Qmsg, true); //第二步：打开连接
+    httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");//设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头）
+    httpRequest.send(title2+data);//发送请求 将情头体写在send中
+};
+ValineButton.onclick=send_valine_Qmsg;
